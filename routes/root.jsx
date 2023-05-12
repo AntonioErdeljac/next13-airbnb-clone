e4e8714917df1319f890414b40dfcf1a96424606
Jesse Hahka <jessehahka@gmail.com>
2023-05-12 08:44:51 -0700

    committed

---
+++ routes/root.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app'; // import the App component from a file
+
+ReactDOM.render(
+  <React.StrictMode>
+    <app/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
